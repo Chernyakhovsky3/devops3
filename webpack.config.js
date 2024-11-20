eefaa0fbb5d6dae7f7a311d3ccf1969177fb99c0
ParalIlya <ivan20051202@gmail.com>
2024-11-20 18:29:57 +0300

    correct front

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // или 'production'
-  entry: 'src/index.js', // Входной файл
+  entry: './src/index.js', // Входной файл
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
